f4a9bb210447f1b56c49dd3ea62dce41f57c9431
Hiago Souza <ohiagossouza@gmail.com>
2024-07-25 17:36:14 -0300

    Initial commit

---
+++ src/main/java/com/hiagosouza/api_santander/SantanderApiApplication.java
@@ -0,0 +1,13 @@
+package com.hiagosouza.api_santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiagosouza/api_santander/SantanderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiagosouza.api_santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
